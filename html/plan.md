d62e4c35807a71eb3294d620d8c89fbcbebce37e
Wiktor <wectorek9@gmail.com>
2025-12-18 20:00:11 +0100

    Added moving by arrows and stop player if it's on the border of the table

---
+++ html/plan.md
@@ -0,0 +1,4 @@
+1. Created table
+2. Added square color
+3. Simple automatic moving in one direction
+4. Moving by arrows
--- js/index.js
+++ js/index.js
@@ -1,4 +1,58 @@
-let x = 0
-let y = 0
-let field = document.getElementById("field-1")
-field.style.backgroundColor="green"
+function setSquareColor(x, y) {
+	let field = document.getElementById("field-" + x + "-" + y);
+	field.style.backgroundColor = "green";
+}
+
+const arena = document.getElementById("arena");
+for (i = 0; i < 10; i++) {
+	const tr = document.createElement("tr");
+	for (j = 0; j < 10; j++) {
+		const td = document.createElement("td");
+		const id = "field-" + j + "-" + i;
+		td.setAttribute("id", id);
+		tr.appendChild(td);
+	}
+	arena.appendChild(tr);
+}
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function moving() {
+	for (i = 0; i < 10; i++) {
+		await sleep(300);
+		setSquareColor(i, 0);
+	}
+}
+let x = 0;
+let y = 0;
+setSquareColor(x, y);
+document.addEventListener("keydown", (event) => {
+	console.log(x, y);
+	const keyName = event.key;
+	removeSquareColor();
+	if (keyName === "ArrowRight") {
+		if (x < 9) {
+			x++;
+			setSquareColor(x, y);
+		} else {
+            setSquareColor(x,y)
+        }
+	}
+	if (keyName === "ArrowLeft") {
+		x--;
+		setSquareColor(x, y);
+	}
+	if (keyName === "ArrowDown") {
+		y++;
+		setSquareColor(x, y);
+	}
+	if (keyName === "ArrowUp") {
+		y--;
+		setSquareColor(x, y);
+	}
+});
+function removeSquareColor() {
+	let field = document.getElementById("field-" + x + "-" + y);
+	field.style.backgroundColor = "black";
+}
